0a1193c9b847b2452368561147f1a1e43e69f96c
sereyvathanasuy <suysereyvathana@gmail.com>
2025-12-19 20:35:09 +0700

    update custom javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,86 @@
 const profileInfo = {
+  webSiteTitle: "Thana.dev",
   phone_number: "+855-969-192-938",
   email: "suysereyvathana@gmail.com",
+  address:
+    "156 Kampong Sambuor, Samraong Knong, Aek Pnhom, Battambang, Cambodia",
   facebookUrl: "https://web.facebook.com/sereyvathanasuy",
   linkedUrl: "https://www.linkedin.com/in/suy-sereyvathana-bb89b1233",
   githubUrl: "https://github.com/sereyvathanasuy",
+  skills: [
+    {
+      title: "Front-End",
+      description: "HTML, CSS, Javascript, Tailwind, Vuejs, Vuetify",
+    },
+    {
+      title: "Back-End",
+      description: "Laravel, ASP.Net, Golang, Fast API",
+    },
+    {
+      title: "Database",
+      description: "MySQL, PostgreSQL, MongoDB",
+    },
+    {
+      title: "Tools & Platforms",
+      description: "Git, Docker",
+    },
+    {
+      title: "Others",
+      description: "RESTful APIs",
+    },
+  ],
+  educations: [
+    {
+      year: "2020-2024",
+      title: "University",
+      subTitle:
+        "Completed Bachelor’s Degree in Information Technology at National University of Battambang",
+    },
+    {
+      year: "2017-2020",
+      title: "High School",
+      subTitle: "Completed at Kdol Doun Teav High School",
+    },
+    {
+      year: "2014-2017",
+      title: "Secondary School",
+      subTitle: "Completed at Kdol Doun Teav Secondary School",
+    },
+    {
+      year: "2008-2014",
+      title: "Primary School",
+      subTitle: "Completed at Norea Primary School",
+    },
+  ],
+  researchs: [
+    {
+      year: "Mar 2023",
+      title: "LBE - Plagiarism Detection",
+      subTitle: "Plagiarism Detection System for Khmer Language.",
+    },
+  ],
+  gitJournalings: [
+    {
+      date: "17 Dec 2025",
+      title: "Parking Management System App",
+    },
+    {
+      date: "15 Aug 2025",
+      title: "Student Management System",
+    },
+    {
+      date: "14 Jun 2025",
+      title: "Ice Factory Management System",
+    },
+    {
+      date: "05 Jan 2025",
+      title: "Accounting, HR, CRM, POS Management System",
+    },
+    {
+      date: "10 Mar 2023",
+      title: "Construction Depot Management System",
+    },
+  ],
 };
 
 const allMenus = [
@@ -40,58 +117,56 @@ const allExternalLinks = [
   },
 ];
 
-const gitJournalings = [
+const programmingIcons = [
+  { title: "Firebase", imagePath: "assets/imgs/home-page-2/hero-1/icon-1.svg" },
+  { title: "MongoDB", imagePath: "assets/imgs/home-page-2/hero-1/icon-2.svg" },
   {
-    date: "17 Dec 2025",
-    title: "Parking Management System App",
+    title: "Postgresql",
+    imagePath: "assets/imgs/home-page-2/hero-1/icon-3.svg",
   },
-  {
-    date: "15 Aug 2025",
-    title: "Student Management System",
-  },
-  {
-    date: "14 Jun 2025",
-    title: "Ice Factory Management System",
-  },
-  {
-    date: "05 Jan 2025",
-    title: "Accounting, HR, CRM, POS Management System",
-  },
-  {
-    date: "10 Mar 2023",
-    title: "Construction Depot Management System",
-  },
+  { title: "Mariadb", imagePath: "assets/imgs/home-page-2/hero-1/icon-4.svg" },
+  { title: "Mysql", imagePath: "assets/imgs/home-page-2/hero-1/icon-5.svg" },
+  { title: "NodeJS", imagePath: "assets/imgs/home-page-2/hero-1/icon-6.svg" },
+  { title: "Tailwind", imagePath: "assets/imgs/home-page-2/hero-1/icon-7.svg" },
+  { title: "Vuejs", imagePath: "assets/imgs/home-page-2/hero-1/icon-8.svg" },
+  { title: "Laravel", imagePath: "assets/imgs/home-page-2/hero-1/icon-9.svg" },
+  { title: "C#", imagePath: "assets/imgs/home-page-2/hero-1/icon-10.svg" },
+  { title: "Dart", imagePath: "assets/imgs/home-page-2/hero-1/icon-11.svg" },
+  { title: "Flutter", imagePath: "assets/imgs/home-page-2/hero-1/icon-12.svg" },
+  { title: "Golang", imagePath: "assets/imgs/home-page-2/hero-1/icon-13.svg" },
+  { title: "NuxtJS", imagePath: "assets/imgs/home-page-2/hero-1/icon-14.svg" },
 ];
 
-const educations = [
+const cooperationSolutions = [
   {
-    year: "2020-2024",
-    title: "University",
-    subTitle:
-      "Completed Bachelor’s Degree in Information Technology at National University of Battambang",
+    icon: "",
+    title: "Web & App Development",
+    description: "",
   },
   {
-    year: "2017-2020",
-    title: "High School",
-    subTitle: "Completed at Kdol Doun Teav High School",
+    icon: "",
+    title: "Database Management",
+    description: "",
   },
   {
-    year: "2014-2017",
-    title: "Secondary School",
-    subTitle: "Completed at Kdol Doun Teav Secondary School",
+    icon: "",
+    title: "API Development",
+    description: "",
   },
   {
-    year: "2008-2014",
-    title: "Primary School",
-    subTitle: "Completed at Norea Primary School",
+    icon: "",
+    title: "Performance Optimization",
+    description: "",
   },
-];
-
-const researchs = [
   {
-    year: "Mar 2023",
-    title: "LBE - Plagiarism Detection",
-    subTitle: "Plagiarism Detection System for Khmer Language.",
+    icon: "",
+    title: "E-commerce Solutions",
+    description: "",
+  },
+  {
+    icon: "",
+    title: "Integrating AI",
+    description: "",
   },
 ];
 
@@ -119,11 +194,62 @@ function loadMain() {
     `;
   });
 
+  // #region Phone Number
+
+  let webSiteTitle = document.getElementsByClassName("website-title");
+
+  Array.from(webSiteTitle).forEach((it) => {
+    it.innerHTML = profileInfo.webSiteTitle;
+  });
+
+  // #region Phone Number
+
+  let phoneNumber = document.getElementsByClassName("phone_number");
+
+  Array.from(phoneNumber).forEach((it) => {
+    it.innerHTML = profileInfo.phone_number;
+  });
+
+  // #region Email
+
+  let emails = document.getElementsByClassName("email");
+
+  Array.from(emails).forEach((it) => {
+    it.innerHTML = profileInfo.email;
+  });
+
+  // #region Address
+
+  let address = document.getElementsByClassName("address");
+
+  Array.from(address).forEach((it) => {
+    it.innerHTML = profileInfo.address;
+  });
+
+  // #region List Programming Icon
+
+  let listProgrammingIcon = document.getElementById("list-programming-icon");
+
+  programmingIcons.forEach((it) => {
+    listProgrammingIcon.innerHTML += `
+      <li class="carouselTicker__item">
+        <a
+          class="brand-logo icon_60 icon-shape rounded-3"
+        >
+          <img
+            src="${it.imagePath}"
+            alt="${it.title}"
+          />
+        </a>
+      </li>
+    `;
+  });
+
   // #region Git Journaling
 
   let gitJournaling = document.getElementById("git-journaling");
 
-  gitJournalings.forEach((it) => {
+  profileInfo.gitJournalings.forEach((it) => {
     gitJournaling.innerHTML += `
         <li class="position-relative z-1">
             <div class="d-flex align-items-center gap-2">
@@ -140,7 +266,7 @@ function loadMain() {
 
   let educationList = document.getElementById("education-list");
 
-  educations.forEach((it) => {
+  profileInfo.educations.forEach((it) => {
     educationList.innerHTML += `
         <li class="position-relative z-1 mb-3">
             <div class="d-flex gap-2">
@@ -164,7 +290,7 @@ function loadMain() {
 
   let researchList = document.getElementById("research-list");
 
-  researchs.forEach((it) => {
+  profileInfo.researchs.forEach((it) => {
     researchList.innerHTML += `
         <li class="position-relative z-1 mb-3">
             <div class="d-flex gap-2">
@@ -181,6 +307,25 @@ function loadMain() {
             </div>
             </div>
         </li>
+    `;
+  });
+
+  // #region All Skill
+
+  let allSkill = document.getElementById("all-skill");
+
+  profileInfo.skills.forEach((it) => {
+    allSkill.innerHTML += `
+      <li class="mb-3">
+        <div
+          class="d-flex flex-column flex-md-row gap-2"
+        >
+          <p class="text-dark text-nowrap mb-0">
+            ${it.title}:
+          </p>
+          <span class="text-300">${it.description}</span>
+        </div>
+      </li>
     `;
   });
 
